5c13c93e8c41d6f81e6df92338a333f268af0d52
Luciano 16-gif <chanominardo10@gmail.com>
2026-03-07 11:08:56 -0400

    first commit

---
+++ src/main/java/ve/edu/unimet/so/project2/project2os/Project2OS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ve.edu.unimet.so.project2.project2os;
+
+/**
+ *
+ * @author chano
+ */
+public class Project2OS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
